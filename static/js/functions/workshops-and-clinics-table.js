886c3e0bd3af68f7bba19f42744680dcd781d847
Edgar A. Negron Rabell <edgar@Edgars-Mac-mini.lan>
2021-01-19 11:38:05 -0400

    Add Data to test DataTables package and begin adding styling for table

---
+++ static/js/functions/workshops-and-clinics-table.js
@@ -0,0 +1,5 @@
+
+const dataTable = new simpleDatatables.DataTable("#workshops-and-clinics-table", {
+	searchable: true,
+	fixedHeight: true,
+})
--- static/js/functions/workshops-and-clinics-tables.js
+++
@@ -1,6 +0,0 @@
-import { DataTable } from "simple-datatables"
-
-const dataTable = new DataTable("#myTable", {
-	searchable: true,
-	fixedHeight: true,
-})
