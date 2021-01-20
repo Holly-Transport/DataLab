cc3dfb5ed027bb6fc9d866a903aedd1cb5c3fde3
Edgar A. Negron Rabell <edgar.negron.rabell@gmail.com>
2021-01-20 12:40:38 -0400

    Add styling changes to improve site design and test sheetrock.js to incorporate Google Sheets into tables

--- static/js/functions/workshops-and-clinics-table.js
+++ static/js/functions/workshops-and-clinics-table.js
@@ -1,5 +1,12 @@
 
-const dataTable = new simpleDatatables.DataTable("#workshops-and-clinics-table", {
-	searchable: true,
-	fixedHeight: true,
+const workshopAndClinicsTable = 'https://docs.google.com/spreadsheets/d/1eYftI0kPrT88aHFlW0n3LNDXlxAlirRsYs2RmbHCea0/edit?usp=sharing'
+const workshopAndClinicsTableElement = document.querySelector('#workshops-and-clinics-table')
+
+workshopAndClinicsTableElement.sheetrock({
+	url: workshopAndClinicsTable,
+	query: "select *"
 })
+// const dataTable = new simpleDatatables.DataTable("", {
+// 	searchable: true,
+// 	fixedHeight: true,
+// })
