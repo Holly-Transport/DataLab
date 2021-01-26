96463108db53d8247895db18e8ff57b3f4627453
Edgar A. Negron Rabell <edgar.negron.rabell@gmail.com>
2021-01-26 15:50:30 -0400

    Improve most recent section styling, add new fontawesome icon

--- static/js/functions/workshops-and-clinics-table.js
+++ static/js/functions/workshops-and-clinics-table.js
@@ -1,25 +1,34 @@
-const tableDates = document.querySelectorAll('.date')
-
-const formatIntoDateType = function (stringDate) {
-	// Convert string date into Date type.
-	if (typeof stringDate === 'string') {
-		return new Date(stringDate).toISOString().split('T')[0] // Formats date into YYYY-MM-DD format.
-	} else {
-		throw new Error("The argument provided is not a string. Please check your arguments type.")
+document.addEventListener('DOMContentLoaded', function() {
+	const mostRecent = document.querySelector('.most-recent')
+	const tableDates = document.querySelectorAll('.date')
+	
+	const formatIntoDateType = function (stringDate) {
+		// Convert string date into Date type.
+		if (typeof stringDate === 'string') {
+			return new Date(stringDate).toISOString().split('T')[0] // Formats date into YYYY-MM-DD format.
+		} else {
+			throw new Error("The argument provided is not a string. Please check your arguments type.")
+		}
 	}
-}
-
-tableDates.forEach(date => {
-	date.textContent = formatIntoDateType(date.textContent)
+	console.log(mostRecent)
+	mostRecent.textContent = formatIntoDateType(mostRecent.textContent)
+	
+	tableDates.forEach(date => {
+		date.textContent = formatIntoDateType(date.textContent)
+	})
+	
+	
+	const dataTable = new simpleDatatables.DataTable("#workshops-and-clinics-table", {
+		searchable: true,
+		fixedHeight: true,
+		columns: [
+			{ select: [1, 3, 4, 5], searchable: false }
+		]
+	})
+	
+	console.log(dataTable)
+	
+	dataTable.on('datatable.search', function(query, matched) {
+		console.log(query, matched)
+	})
 })
-
-const dataTable = new simpleDatatables.DataTable("#workshops-and-clinics-table", {
-	searchable: true,
-	fixedHeight: true,
-	columns: [
-		{ select: [1, 3, 4, 5], searchable: false }
-	]
-})
-
-console.log(dataTable)
-
