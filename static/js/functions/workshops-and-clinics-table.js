79dd546ea0dd7c28e61ac0bc10931ba0d3a487e3
Edgar A. Negron Rabell <edgar.negron.rabell@gmail.com>
2021-02-08 09:19:14 -0400

    Revert previous changes that did not fix deployment issue

--- static/js/functions/workshops-and-clinics-table.js
+++ static/js/functions/workshops-and-clinics-table.js
@@ -1,28 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
-	const mostRecent = document.querySelector('.most-recent')
-	const tableDates = document.querySelectorAll('.date')
-	
-	const formatIntoDateType = function (stringDate) {
-		// Convert string date into Date type.
-		if (typeof stringDate === 'string') {
-			return new Date(stringDate).toISOString().split('T')[0] // Formats date into YYYY-MM-DD format.
-		} else {
-			throw new Error("The argument provided is not a string. Please check your arguments type.")
-		}
-	}
-	if(mostRecent) {
-		mostRecent.textContent = formatIntoDateType(mostRecent.textContent)
-	}
-	
-	tableDates.forEach(date => {
-		date.textContent = formatIntoDateType(date.textContent)
-	})
-	
-	if (document.getElementById('workshops-and-clinics-table')) {
-		const dataTable = new simpleDatatables.DataTable("#workshops-and-clinics-table", {
-			searchable: true,
-			fixedHeight: true,
-			perPage: 5
-		})
-	}
-})
+document.addEventListener("DOMContentLoaded", function () {
+  const mostRecent = document.querySelector(".most-recent");
+  const tableDates = document.querySelectorAll(".date");
+
+  const formatIntoDateType = function (stringDate) {
+    // Convert string date into Date type.
+    if (typeof stringDate === "string") {
+      return new Date(stringDate).toISOString().split("T")[0]; // Formats date into YYYY-MM-DD format.
+    } else {
+      throw new Error(
+        "The argument provided is not a string. Please check your arguments type."
+      );
+    }
+  };
+  if (mostRecent) {
+    mostRecent.textContent = formatIntoDateType(mostRecent.textContent);
+  }
+
+  tableDates.forEach((date) => {
+    date.textContent = formatIntoDateType(date.textContent);
+  });
+
+  const dataTable = new simpleDatatables.DataTable(
+    "#workshops-and-clinics-table",
+    {
+      searchable: true,
+      fixedHeight: true,
+      perPage: 5,
+    }
+  );
+});
