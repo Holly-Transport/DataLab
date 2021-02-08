5cb66aa56b57e87c05a4c25d4b2c0a4d3e55f1a7
Edgar A. Negron Rabell <edgar.negron.rabell@gmail.com>
2021-02-08 09:34:19 -0400

    Make small adjustments to find deployment bug

--- static/js/functions/workshops-and-clinics-table.js
+++ static/js/functions/workshops-and-clinics-table.js
@@ -1,31 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const mostRecent = document.querySelector(".most-recent");
-  const tableDates = document.querySelectorAll(".date");
+  const mostRecent = document.querySelector(".most-recent")
+  const tableDates = document.querySelectorAll(".date")
 
   const formatIntoDateType = function (stringDate) {
     // Convert string date into Date type.
     if (typeof stringDate === "string") {
-      return new Date(stringDate).toISOString().split("T")[0]; // Formats date into YYYY-MM-DD format.
+      return new Date(stringDate).toISOString().split("T")[0] // Formats date into YYYY-MM-DD format.
     } else {
       throw new Error(
         "The argument provided is not a string. Please check your arguments type."
-      );
+      )
     }
-  };
+  }
   if (mostRecent) {
-    mostRecent.textContent = formatIntoDateType(mostRecent.textContent);
+    mostRecent.textContent = formatIntoDateType(mostRecent.textContent)
   }
 
   tableDates.forEach((date) => {
-    date.textContent = formatIntoDateType(date.textContent);
-  });
+    date.textContent = formatIntoDateType(date.textContent)
+  })
 
   const dataTable = new simpleDatatables.DataTable(
-    "#workshops-and-clinics-table",
-    {
+    "#workshops-and-clinics-table", {
       searchable: true,
       fixedHeight: true,
       perPage: 5,
     }
-  );
-});
+  )
+})
